c575a16afcc7ca2638648bda72059879f0fea409
K Prashant <k.prashant89@gmail.com>
2017-02-16 22:46:54 +0530

    A demo C++ Program
    A demo C++ file added for learning purpose

---
+++ cc1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int a,b,c;
+    cin>>a>>b;
+    c=a-b;
+    if((c+1)%10==0)
+    cout<<c-1;
+    else
+    cout<<c+1;
+
+    cout<<endl;
+
+    return 0;
+
+}
